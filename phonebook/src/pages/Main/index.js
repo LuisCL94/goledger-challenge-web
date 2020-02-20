e80eb876a3c18e02ccff1773419a11ba18a4d93c
Luis Claudio Telles Lima <lclaudio.tl@gmail.com>
2020-02-20 15:44:57 -0300

    Initiate project and setting up structure

---
+++ phonebook/src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Main extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello Main Page</h1>
+      </>
+    );
+  }
+}
+
+export default Main;
